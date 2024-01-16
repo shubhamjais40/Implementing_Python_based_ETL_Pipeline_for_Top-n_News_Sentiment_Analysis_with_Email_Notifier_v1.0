2e9d8d729861fdadc3112994541f342456f57b18
shubham <shubhamkjais40@gmai.com>
2024-01-16 20:04:27 +0530

    job scheduler added

---
+++ CRON_Job_scheduler.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/hadoop/etl_project/Python_ETL_topnews_with_Sentiments/
+python3 etl_run.py
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "setting up python dependencies..."
+
+
+pip3 install -r requirements.txt
